bf15d2e94b1b310e6583683b04b23aaaddf40ab1
unknown <20190401@cyadmk.com>
2020-03-03 16:34:04 +0800

    update Q3

--- src/exam/Q2-TwoFer.ts
+++ src/exam/Q2-TwoFer.ts
@@ -1,4 +1,4 @@
-// 呼叫 TwoFer.twoFer() 時
+// 使用 TwoFer.twoFer() 時
 // TODO 1. 如果有傳入名稱字串，則回傳 "One for {name}, one for me.
 // TODO 2. 回傳的 name 一定必須大寫開頭
 // TODO 3. 如果沒有傳入名稱，則回傳 "One for you, one for me."
---
+++ src/exam/Q3-Resistor-Color.ts
@@ -0,0 +1,26 @@
+// 此程式目的為輸入兩種顏色的英文名稱，並轉化為兩位數字以便讓神秘的機器識別...
+// 透過 const resistorColor = new ResistorColor(["brown", "black"])
+// 呼叫 resistorColor.value() 回傳對應的色碼
+// 色碼表 0 ~ 9 => Black - Brown - Red - Orange - Yellow - Green - Blue - Violet - Grey - White
+// TODO 1. 如果是 brown, black 應該得到 10
+// TODO 2. 如果不小心輸入三種顏色，自動略過第三個，一樣輸出兩位數字
+// TODO 3. 如果傳入的顏色不足兩個，則拋出例外
+// TODO 4. Hint 除了必須利用以下基礎結構外，別忘了再自行增加色碼對應表
+class ResistorColor {
+    private colors: string[];
+
+    constructor(colors: string[]) {
+        // Code here ! 從這裡開始變更
+        this.colors = colors;
+    }
+
+    // Code here ! 從這裡開始變更
+    value = (): number => 0;
+}
+
+// Export for test，請無視
+export default ResistorColor
+
+// Use for debug
+// const resistorColor = new ResistorColor(["brown", "black"]
+// console.log(resistorColor.value())
